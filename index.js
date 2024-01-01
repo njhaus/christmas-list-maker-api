ffeb8176f687e1d95d3ff6764a69030841f07f46
njhaus <njhaus@gmail.com>
2024-01-01 14:17:52 -0600

    Update cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT;
 
 // const origin = "http://localhost:5173";
 const origin =
-  "https://christmas-list-maker-production.up.railway.app/home";
+  "https://christmas-list-maker-production.up.railway.app";
 // cors middleware for allowing react to fetch() from server
 var cors = require("cors");
 app.use(
